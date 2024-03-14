e87a33b6a37a9ced92069217895bb2a91da6ae2b
João Pedro Cavalaro <ticavalaro@gmail.com>
2024-03-14 11:08:07 -0300

    Database configuration

--- CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
+++ CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b292c15917ddaae3735ddc3d153f91d8090fbbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a0b845aad16f03eff3a9bf1b9f8d70485c3929")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
